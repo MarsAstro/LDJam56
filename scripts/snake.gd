10b32a302fba835d19b0547fac0d58e77b8f89fd
Marius Stubberud <marius.stubberud.92@gmail.com>
2024-10-06 14:06:15 +0200

    mådels

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -1,15 +1,24 @@
 extends Node3D
 
-@export var snake_head: Node3D
-@export var snake_front_body: Node3D
-@export var snake_back_body: Node3D
-@export var snake_tail: Node3D
+@export var snake_body: SnakeBody
 @export var snake_mesh: MeshInstance3D
+
+@export var slither_snake_front_body: Node3D
+@export var slither_snake_back_body: Node3D
+@export var slither_snake_tail: Node3D
+
+@export var coil_snake_front_body: Node3D
+@export var coil_snake_back_body: Node3D
+@export var coil_snake_tail: Node3D
 
 func _process(_delta: float) -> void:
 	var material := snake_mesh.mesh.surface_get_material(0) as ShaderMaterial
-
-	material.set_shader_parameter("snake_head", snake_head.global_position)
-	material.set_shader_parameter("snake_front_body", snake_front_body.global_position)
-	material.set_shader_parameter("snake_back_body", snake_back_body.global_position)
-	material.set_shader_parameter("snake_tail", snake_tail.global_position)
+	material.set_shader_parameter("snake_head", snake_body.global_position)
+	
+	var front_pos = slither_snake_front_body.global_position.slerp(coil_snake_front_body.global_position, snake_body.coil_amount)
+	var back_pos = slither_snake_back_body.global_position.slerp(coil_snake_back_body.global_position, snake_body.coil_amount)
+	var tail_pos = slither_snake_tail.global_position.slerp(coil_snake_tail.global_position, snake_body.coil_amount)
+	
+	material.set_shader_parameter("snake_front_body", front_pos)
+	material.set_shader_parameter("snake_back_body", back_pos)
+	material.set_shader_parameter("snake_tail", tail_pos)
---
+++ scripts/snake_body.gd
@@ -0,0 +1,85 @@
+class_name SnakeBody
+extends CharacterBody3D
+
+const WALK_SPEED = 8.0
+const COIL_SPEED = 12.0
+const JUMP_VELOCITY: float = 6
+const MOUSE_SENSITIVITY: float = 0.05
+const GAMEPAD_SENSITIVITY: float = 2
+const DEADZONE: float = 0.1
+
+var speed: float
+var coil_amount: float
+
+@export var pivot: Node3D
+@export var camera: Camera3D
+@export var coiling_speed: float = 4
+
+@onready var coil_collider: CollisionShape3D = $CoilCollider
+@onready var coil: MeshInstance3D = $CoilMesh
+@onready var head_collider: CollisionShape3D = $HeadCollider
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
+		pivot.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
+		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-60), deg_to_rad(15))
+
+
+func _process(delta: float) -> void:
+	var cameraHorizontalInput: float = Input.get_axis("camera_right", "camera_left")
+	var cameraVerticalInput: float = Input.get_axis("camera_down", "camera_up")
+	
+	if cameraHorizontalInput > DEADZONE or cameraHorizontalInput < -DEADZONE:
+		rotate_y(cameraHorizontalInput * delta * GAMEPAD_SENSITIVITY)
+	if cameraVerticalInput > DEADZONE or cameraVerticalInput < -DEADZONE:
+		pivot.rotate_x(cameraVerticalInput * delta * GAMEPAD_SENSITIVITY)
+		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-60), deg_to_rad(15))
+		
+	if Input.is_action_pressed("coil"):
+		coil_amount = clamp(coil_amount + delta * coiling_speed, 0.0, 1.0)
+	else:
+		coil_amount = clamp(coil_amount - delta * coiling_speed, 0.0, 1.0)
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and coil_amount >= 1.0 and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if is_on_floor():
+		if coil_amount >= 1.0:
+			speed = COIL_SPEED
+			head_collider.disabled = true
+			coil_collider.disabled = false
+			coil.visible = false
+		else:
+			speed = WALK_SPEED
+			head_collider.disabled = false
+			coil_collider.disabled = true
+			coil.visible = false
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_right", "move_left", "move_backward", "move_forward")
+	var direction := (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
+
+	move_and_slide()
