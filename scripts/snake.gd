02145ff4e3a0089cd205d443d605b281775f50ea
Marius Stubberud <marius.stubberud.92@gmail.com>
2024-10-06 14:44:19 +0200

    testing

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -11,10 +11,13 @@ extends Node3D
 @export var coil_snake_back_body: Node3D
 @export var coil_snake_tail: Node3D
 
+@export var body_path: Path3D
+
 func _process(_delta: float) -> void:
 	#var material := snake_mesh.mesh.surface_get_material(0) as ShaderMaterial
 	#material.set_shader_parameter("snake_head", snake_body.global_position)
 	
+	var head_pos = snake_body.global_position
 	var front_pos = slither_snake_front_body.global_position.slerp(coil_snake_front_body.global_position, snake_body.coil_amount)
 	var back_pos = slither_snake_back_body.global_position.slerp(coil_snake_back_body.global_position, snake_body.coil_amount)
 	var tail_pos = slither_snake_tail.global_position.slerp(coil_snake_tail.global_position, snake_body.coil_amount)
