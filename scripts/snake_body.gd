76631dc9725a6feb39a5d8da09862db187367ab1
Marius Stubberud <marius.stubberud.92@gmail.com>
2024-10-06 21:57:24 +0200

    Made snake move better and rotate

--- scripts/snake_body.gd
+++ scripts/snake_body.gd
@@ -11,8 +11,9 @@ const DEADZONE: float = 0.1
 var speed: float
 var coil_amount: float
 
-@export var pivot: Node3D
+@export var camera_pivot: Node3D
 @export var camera: Camera3D
+@export var coil_pivot: Node3D
 @export var coiling_speed: float = 4
 
 @onready var coil_collider: CollisionShape3D = $CoilCollider
@@ -25,8 +26,8 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
-		pivot.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
-		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-60), deg_to_rad(15))
+		camera_pivot.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
+		camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, deg_to_rad(-60), deg_to_rad(15))
 
 
 func _process(delta: float) -> void:
@@ -36,13 +37,18 @@ func _process(delta: float) -> void:
 	if cameraHorizontalInput > DEADZONE or cameraHorizontalInput < -DEADZONE:
 		rotate_y(cameraHorizontalInput * delta * GAMEPAD_SENSITIVITY)
 	if cameraVerticalInput > DEADZONE or cameraVerticalInput < -DEADZONE:
-		pivot.rotate_x(cameraVerticalInput * delta * GAMEPAD_SENSITIVITY)
-		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-60), deg_to_rad(15))
+		camera_pivot.rotate_x(cameraVerticalInput * delta * GAMEPAD_SENSITIVITY)
+		camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, deg_to_rad(-60), deg_to_rad(15))
 		
 	if Input.is_action_pressed("coil"):
 		coil_amount = clamp(coil_amount + delta * coiling_speed, 0.0, 1.0)
+		coil_pivot.rotate_x(-delta * 10.0 * coil_amount)
 	else:
 		coil_amount = clamp(coil_amount - delta * coiling_speed, 0.0, 1.0)
+		var current_x_rot = coil_pivot.rotation.x
+		
+		if (current_x_rot >= 0.01 or current_x_rot <= -0.01):
+			coil_pivot.rotation.x = move_toward(current_x_rot, 0.0, delta * 10.0)
 
 
 func _physics_process(delta: float) -> void:
@@ -60,9 +66,10 @@ func _physics_process(delta: float) -> void:
 		else:
 			head_collider.disabled = false
 			coil_collider.disabled = true
-
+	
 	var input_dir := Input.get_vector("move_right", "move_left", "move_backward", "move_forward")
-	var direction := (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
+	var strafe_speed: float = 0.0 if Input.is_action_pressed("coil") else 1.0
+	var direction := (transform.basis * Vector3(-input_dir.x * strafe_speed, 0, -input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * speed
