389ad1bf9f945fd06cbf435de654eec429958b5a
Marius Stubberud <marius.stubberud.92@gmail.com>
2024-10-13 08:17:50 +0200

    Made snake less climby and eggs magnetized

--- scripts/egg.gd
+++ scripts/egg.gd
@@ -1,6 +1,20 @@
 class_name Egg
 extends Area3D
 
-func _on_body_entered(_body: SnakeBody) -> void:
+@export var magnet_speed: float = 5
+
+var has_target: bool = false
+var target: SnakeBody
+
+func _process(delta: float) -> void:
+	if has_target:
+		var direction = (target.global_position - position).normalized()
+		position += direction * magnet_speed * delta
+
+func _on_body_entered(body: SnakeBody) -> void:
+	has_target = true
+	target = body
+
+func _on_collection_hitbox_body_entered(_body: SnakeBody) -> void:
 	GameManager.eat_egg()
 	queue_free()
